6f65b0d1718602330d961f9999ab45dae003d7ef
VeraBakmaz <vera.bakmaz@gmail.com>
2017-11-08 12:25:34 +0100

    Initial commit.

---
+++ src/components/game.js
@@ -0,0 +1,13 @@
+((() => {
+    const html = `
+      <div class="game">
+        <h1>tandara mandara</h1>
+        <!-- place your app components here -->
+      </div>
+    `
+  
+    Vue.component("game", {
+      template: html
+    })
+  }))()
+  
